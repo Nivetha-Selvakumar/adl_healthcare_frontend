00eb9f38c7401a78ea2547e8129a9dd2a9d48d26
Nivetha-Selvakumar <nivetha.s@aaludra.com>
2026-05-14 14:57:55 +0530

    Initial React Vite TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-100 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-700">
+        MediAssist Healthcare App
+      </h1>
+    </div>
+  )
+}
+
+export default App
